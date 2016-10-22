319a5dd0c994506c066ab3f3357d18961980af2d
colinski <colnski03@gmail.com>
2016-10-21 22:43:34 -0500

    update

--- knn_learner.py
+++ knn_learner.py
@@ -1,20 +1,12 @@
-import math, sys
+import sys, math
 from feature import Feature
 from sorted_array import SortedArray
-from collections import OrderedDict
 
-def knn_learn(train_instances, test_instances, k, labels, doPrint):
-	mode = 'r' if labels == [] else 'c'
+def knn_learn(train_instances, test_instances, k, labels, mode, doPrint):
 	err = 0.0
 	for i in range(len(test_instances)):
 		q = test_instances[i]
 		neighbors = find_nearest_neighbors(q, k, train_instances)
-
-		'''
-		for n in neighbors:
-			print get_label(n[0], mode)
-		'''
-
 		if mode == 'r':
 			avg = 0.0
 			for n in neighbors:
@@ -25,22 +17,23 @@ def knn_learn(train_instances, test_instances, k, labels, doPrint):
 				print('Predicted value : ' + '{0:.6f}'.format(avg)
 					+ '\tActual value : '  + '{0:.6f}'.format(get_label(q, mode)))
 		else:
-			guess = predict_class(neighbors, q, labels)
+			guess = predict_class(neighbors, labels)
 			if guess != get_label(q, mode):
 				err += 1
 			if doPrint:
-				print 'Predicted class : ' + guess + '\t Actual class : ' + get_label(q, mode)
-
+				print 'Predicted class : ' + guess + '\tActual class : ' + get_label(q, mode)
+	
 	if mode == 'r':
 		if doPrint:
-			print 'Mean absolute error : ' + '{0:.16f}'.format((err / len(test_instances)))
-			print 'Total number of instances : ' + str(len(test_instances))
+			print('Mean absolute error : ' + '{0:.16f}'.format((err / len(test_instances))) 
+				+ '\nTotal number of instances : ' + str(len(test_instances)))
 		return err / len(test_instances)
 	else:
 		if doPrint:
-			print 'err is ' + str(err)
-			print 'got ' + str(len(test_instances) - err) + ' right'
-			print 'total ' + str(len(test_instances))
+			num_correct = int(len(test_instances) - err)
+			print('Number of correctly classified instances : ' + str(num_correct)
+				+ '\nTotal number of instances : ' + str(len(test_instances)) 
+				+ '\nAccuracy : ' + '{0:.16f}'.format((float(num_correct) / len(test_instances))))
 		return err
 
 def find_nearest_neighbors(q, k, train_instances):
@@ -56,19 +49,13 @@ def calc_dist(v, w):
 		dist += (float(v[i]) - float(w[i])) ** 2.0
 	return math.sqrt(dist)
 
-def get_label(n, mode):
-	val = n[len(n) - 1]
-	return float(val) if mode == 'r' else str(val)
-
-def predict_class(neighbors, q, labels):
+def predict_class(neighbors, labels):
 	neighbor_sets = {}
 	for l in labels:
 		neighbor_sets[l] = 0
 	
 	for n in neighbors:
 		label = get_label(n[0], 'c')
-		#if not neighbor_sets.has_key(label):
-		#	neighbor_sets[label] = 0
 		neighbor_sets[label] += 1
 
 	best = 0
@@ -76,7 +63,7 @@ def predict_class(neighbors, q, labels):
 		if neighbor_sets[key] > best:
 			best = neighbor_sets[key]
 	
-	best_index = float('inf')s
+	best_index = float('inf')
 	best_label = ''
 	for key in neighbor_sets.keys():
 		if neighbor_sets[key] == best:
@@ -84,58 +71,11 @@ def predict_class(neighbors, q, labels):
 			if index < best_index:
 				best_index = index
 				best_label = key
-			#del neighbor_sets[key]
 	return best_label
 
-	#return neighbor_sets.keys()[0]
-
-	
-	'''
-	for key in neighbor_sets:
-		index = label_to_index(key, labels)
-		if index < best_index:
-			best_index = index
-			best_label = key
-	return best_label
-	'''
-	
-
-'''
-	for key in neighbor_sets:
-	if len(neighbor_sets.keys()) == 1:
-		#print 'returning because size is 1'
-
-		return get_label(neighbor_sets[0][0], 'c')
-
-
-	best_index = float('inf')
-	best_label = ''
-	for n in best_neighbors:
-		index = label_to_index(n[0], labels)
-		if index < best_index:
-			best_index = index
-			best_label = get_label(n[0], 'c')
-	return best_label
-
-
-	best_neighbors.sort(key = lambda x : x[1])
-	best_dist = float(best_neighbors[0][1])
-	closest_neighbors = []
-	for n in best_neighbors:
-		if n[1] == best_dist:
-			closest_neighbors.append(n)
-
-	best_index = 10000
-	best_label = ''
-	for n in closest_neighbors:
-		index = label_to_index(n[0], labels)
-		if index < best_index:
-			best_index = index
-			best_label = get_label(n[0], 'c')
-	#closest_neighbors.sort(key = lambda n : label_to_index(n[0], features))
-	return best_label
-'''
-
+def get_label(n, mode):
+	val = n[len(n) - 1]
+	return float(val) if mode == 'r' else str(val)
 
 def label_to_index(l, labels):
 	for i in range(len(labels)):
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, copy
+import sys
 from arff_parser import ArffParser
 import knn_learner as kl
 
@@ -6,29 +6,36 @@ def main():
 	if len(sys.argv) == 4:
 		(train_instances, features, labels) = ArffParser(sys.argv[1]).parse()
 		test_instances = ArffParser(sys.argv[2]).parse()[0]
+		mode = 'r' if labels == [] else 'c'
 		k = int(sys.argv[3])
-		err = kl.knn_learn(train_instances, test_instances, k, labels, True)
-		print err
+		print 'k value : ' + str(k)
+		err = kl.knn_learn(train_instances, test_instances, k, labels, mode, True)
 
 	elif len(sys.argv) == 6:
 		(train_instances, features, labels) = ArffParser(sys.argv[1]).parse()
-		print labels
 		test_instances = ArffParser(sys.argv[2]).parse()[0]
+		mode = 'r' if labels == [] else 'c'
 		kvalues = [int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])]
+		best_k = -1
+		lowest_err = float('inf')
 		for k in kvalues:
 			err = 0.0
 			for i in range(len(train_instances)):
-				train_copy = []
-				inst = None
-				for j in range(len(train_instances)):
-					if j != i:
-						train_copy.append(train_instances[j])
-					else:
-						inst = train_instances[j]
-				#train_copy = copy.deepcopy(train_instances)
-				#inst = train_copy.pop(i)
-				err += kl.knn_learn(train_copy, [inst], k, labels, False)
-			print 'err for k = ' + str(k) + ' is ' + str(err)
+				train_copy = list(train_instances)
+				inst = train_copy.pop(i)
+				err += kl.knn_learn(train_copy, [inst], k, labels, mode, False)
+			if err < lowest_err:
+				lowest_err = err
+				best_k = k
+			if mode == 'r':
+				print('Mean absolute error for k = ' + str(k) + ' : ' 
+					+ '{0:.16f}'.format(err / len(train_instances)))
+			else:
+				print('Number of incorrectly classified instances for k = ' 
+					+ str(k) + ' : ' + str(int(err)))
+		print 'Best k value : ' + str(best_k)
+		kl.knn_learn(train_instances, test_instances, best_k, labels, mode, True)
+	
 	else:
 		print 'incorrect usage'
 
